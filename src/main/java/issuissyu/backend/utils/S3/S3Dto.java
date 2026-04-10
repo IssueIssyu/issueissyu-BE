2fb729be588e02563f092b29792b9ec3c594d675
selnem <tjdghks0130@naver.com>
2026-04-10 19:20:59 +0900

    feat: global, dotenv, swagger, s3Util setting: postrge, springboot mvc, jpa, jwt, oauth, aws

---
+++ src/main/java/issuissyu/backend/utils/S3/S3Dto.java
@@ -0,0 +1,11 @@
+package issuissyu.backend.utils.S3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class S3Dto {
+    private final String url;
+    private final String key;
+}
