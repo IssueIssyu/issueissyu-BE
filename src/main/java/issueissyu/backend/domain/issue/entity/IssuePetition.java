7df69fb4f3965f2cd24e7d6afd470baeaa49f643
taerimiiii <shipton0201@gmail.com>
2026-04-26 04:32:22 +0900

    feat: in절 활용

--- src/main/java/issueissyu/backend/domain/issue/entity/IssuePetition.java
+++ src/main/java/issueissyu/backend/domain/issue/entity/IssuePetition.java
@@ -2,13 +2,7 @@ package issueissyu.backend.domain.issue.entity;
 
 import issueissyu.backend.domain.user.entity.User;
 import issueissyu.backend.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
