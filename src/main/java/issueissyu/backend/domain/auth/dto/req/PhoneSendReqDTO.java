171efedc4360eea858e2d26ba9ed8a85ac58a225
taerimiiii <shipton0201@gmail.com>
2026-04-28 01:06:27 +0900

    feat: 전화번호 인증 수정

--- src/main/java/issueissyu/backend/domain/auth/dto/req/PhoneSendReqDTO.java
+++ src/main/java/issueissyu/backend/domain/auth/dto/req/PhoneSendReqDTO.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PhoneSendReqDTO {
 
+    @Schema(example = "010-1234-5678")
     @NotBlank(message = "전화번호를 입력해주세요.")
     @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호 형식은 010-XXXX-XXXX 이어야 합니다.")
     private String phone;
--- src/main/java/issueissyu/backend/domain/auth/dto/req/PhoneVerifyReqDTO.java
+++ src/main/java/issueissyu/backend/domain/auth/dto/req/PhoneVerifyReqDTO.java
@@ -1,6 +1,7 @@
 package issueissyu.backend.domain.auth.dto.req;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -10,14 +11,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PhoneVerifyReqDTO {
 
+    @Schema(example = "010-1234-5678")
     @NotBlank(message = "전화번호를 입력해주세요.")
     @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호 형식은 010-XXXX-XXXX 이어야 합니다.")
     private String phone;
 
+    @Schema(example = "123456")
     @NotBlank(message = "인증번호를 입력해주세요.")
     @Pattern(regexp = "^\\d{6}$", message = "인증번호는 6자리 숫자입니다.")
     private String code;
 
+    @Schema(example = "true")
     @JsonProperty("is_available_nickname")
     private Boolean isAvailableNickname;
 }
