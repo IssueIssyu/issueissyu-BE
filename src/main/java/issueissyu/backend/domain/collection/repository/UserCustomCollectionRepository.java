90e7b7efefbdc0e131c9060e025b70d849cc9a6b
taerimiiii <shipton0201@gmail.com>
2026-05-04 04:11:43 +0900

    feat: 기본 프로필 추가

--- src/main/java/issueissyu/backend/domain/collection/repository/UserCustomCollectionRepository.java
+++ src/main/java/issueissyu/backend/domain/collection/repository/UserCustomCollectionRepository.java
@@ -1,4 +1,11 @@
 package issueissyu.backend.domain.collection.repository;
 
-public interface UserCustomCollectionRepository {
+import issueissyu.backend.domain.collection.entity.mapping.UserCustomCollection;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserCustomCollectionRepository extends JpaRepository<UserCustomCollection, Long> {
+
+    Optional<UserCustomCollection> findByUser_UidAndCustomCollection_CustomCollectionName(
+            String uid, String customCollectionName);
 }
