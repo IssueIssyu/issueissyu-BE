62cb249161df29dade745837d0e7ca8a85f7235a
taerimiiii <shipton0201@gmail.com>
2026-05-05 02:48:58 +0900

    feat: 응답 DTO에 프로퍼티 추가

--- src/main/java/issueissyu/backend/domain/map/dto/res/MapNoticeItemResDTO.java
+++ src/main/java/issueissyu/backend/domain/map/dto/res/MapNoticeItemResDTO.java
@@ -5,4 +5,7 @@ import lombok.Builder;
 
 @Builder
 public record MapNoticeItemResDTO(
-        @JsonProperty("notice_id") long noticeId, long pinId, String noticeContent) {}
+        @JsonProperty("notice_id") long noticeId,
+        @JsonProperty("pin_id") long pinId,
+        @JsonProperty("notice_content") String noticeContent
+) {}
