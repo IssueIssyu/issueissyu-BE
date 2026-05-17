d3ba725adef5de352a2c4a13790e058bc6b3d794
taerimiiii <shipton0201@gmail.com>
2026-05-17 17:58:06 +0900

    bug: 로컬 로그인 에러 코드 응답 수정

--- src/main/java/issueissyu/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/issueissyu/backend/global/exception/GlobalExceptionHandler.java
@@ -75,6 +75,11 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         BaseErrorCode errorCode = GeneralErrorCode.BAD_REQUEST;
         if (request instanceof ServletWebRequest servletWebRequest) {
             String uri = servletWebRequest.getRequest().getRequestURI();
+            if ("/auth/login/local".equals(uri)) {
+                ApiResponse<Void> body = ApiResponse.onFailure(AuthErrorCode.LOCAL_LOGIN_401_1);
+                return super.handleExceptionInternal(e, body, HttpHeaders.EMPTY,
+                        AuthErrorCode.LOCAL_LOGIN_401_1.getReason().getHttpStatus(), request);
+            }
             if ("/auth/signup/local".equals(uri)) {
                 errorCode = AuthErrorCode.LOCAL_SIGNUP_400_1;
             } else if ("/api/auth/phone/send".equals(uri)) {
